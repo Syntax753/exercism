2843a77a41507a09a5a108353f9c4077f3e1abe9
Peter Turner <peter.turner@hcl.com>
2019-12-17 19:28:01 +0000

    Working solution using stream filter for ' ' and '-'

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        Map<String, Long> freq = phrase.toLowerCase().chars().mapToObj(String::valueOf).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        Map<String, Long> freq = phrase.toLowerCase().chars().filter(c -> c != ' ' && c != '-').mapToObj(String::valueOf).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
         boolean repeat = freq.values().stream().anyMatch(cnt -> cnt > 1);
         return !repeat;
