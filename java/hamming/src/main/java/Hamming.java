426fb3ec78d46def1dbbcaec2384f973f7c5bb92
Peter Turner <peter.turner@hcl.com>
2019-12-18 06:25:10 +0000

    Hamming working solution

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,31 @@
 class Hamming {
 
+    private String leftStrand;
+    private String rightStrand;
+    private int ham;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        if (!(leftStrand.isEmpty() && rightStrand.isEmpty()))
+            if (leftStrand.isEmpty()) {
+                throw new IllegalArgumentException("left strand must not be empty.");
+            } else if (rightStrand.isEmpty()) {
+                throw new IllegalArgumentException("right strand must not be empty.");
+            } else if (leftStrand.length() != rightStrand.length()) {
+                throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+            }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.codePointAt(i) != rightStrand.codePointAt(i)) {
+                ham++;
+            }
+        }
 
+        return ham;
+    }
 }
